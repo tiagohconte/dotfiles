1f790fae7cac6a381b8c5d91cddb3d1be0133b38
Tiago Conte <tiagohconte@gmail.com>
2022-07-17 15:39:08 -0300

    Small tweaks on player-info

--- polybar-scripts/player-info.sh
+++ polybar-scripts/player-info.sh
@@ -5,7 +5,7 @@ player_status=$(playerctl status 2> /dev/null)
 if [ "$player_status" = "Playing" ]; then
     echo "🎵 $(playerctl metadata artist) - $(playerctl metadata title)"
 elif [ "$player_status" = "Paused" ]; then
-    echo "#5111B $(playerctl metadata artist) - $(playerctl metadata title)"
+    echo "$(playerctl metadata artist) - $(playerctl metadata title)"
 else
-    echo "#3"
+    echo "Nothing playing :("
 fi
