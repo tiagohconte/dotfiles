6a7e868c1811a9c96fbedac2f9a6a57c94db0ec8
Tiago Conte <tiagohconte@gmail.com>
2022-07-17 16:02:39 -0300

    Changed player info to Spotify info

---
+++ polybar-scripts/spotify-info.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+player_status=$(playerctl --player=spotify status 2> /dev/null)
+
+if [ "$player_status" = "Playing" ]; then
+    echo "🎵 $(playerctl --player=spotify metadata artist) - $(playerctl --player=spotify metadata title)"
+elif [ "$player_status" = "Paused" ]; then
+    echo "$(playerctl --player=spotify metadata artist) - $(playerctl --player=spotify metadata title)"
+else
+    echo "Spotify"
+fi
