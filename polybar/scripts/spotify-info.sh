b948c7b4d2ef18c683f2d5b61c0daa0f6984d390
Tiago Conte <dev.tiagohconte@gmail.com>
2024-03-08 21:24:07 -0300

    fix(polybar): Update Spotify info script

--- polybar/scripts/spotify-info.sh
+++ polybar/scripts/spotify-info.sh
@@ -2,9 +2,7 @@
 
 player_status=$(playerctl --player=spotify status 2> /dev/null)
 
-if [ "$player_status" = "Playing" ]; then
-    echo "$(playerctl --player=spotify metadata artist) - $(playerctl --player=spotify metadata title)"
-elif [ "$player_status" = "Paused" ]; then
+if [[ "$player_status" =~ ^(Playing|Paused)$ ]]; then
     echo "$(playerctl --player=spotify metadata artist) - $(playerctl --player=spotify metadata title)"
 else
     echo "Spotify"
